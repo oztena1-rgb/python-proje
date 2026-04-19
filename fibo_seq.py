9cb640889fe529434795f08a9918ed361256c69d
Alp Özten <oztena1@gmail.com>
2026-04-19 18:53:19 +0300

    fibo_seq.py güncellendi

---
+++ fibo_seq.py
@@ -0,0 +1,16 @@
+def fibo(n):
+    if n==1:
+        return [1]
+    if n==2:
+        return [1,1]
+    # n > 2
+    a = 1
+    b = 1
+    # First two members of the series
+    series = [a, b]
+    for i in range(n):
+        c = a + b
+        series.append(c)
+        a = b
+        b = c
+    return series
