8b8beb8316ed30cb38d5ab37b18c42a2dddbf527
Alp Özten <oztena1@gmail.com>
2026-05-01 20:32:09 +0300

    list.py dosyası eklendi

--- list.py
+++ list.py
@@ -1,2 +1,5 @@
-bicycle = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}."
+
+print(message)
+
